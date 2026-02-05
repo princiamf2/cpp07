c8cb1822b2bd6fe43be9831d2f26cc1d83f229cf
princiamf2 <mmfuri6@gmail.com>
2026-02-05 02:04:25 +0100

    cpp07

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: michel <michel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 01:42:21 by michel            #+#    #+#             */
-/*   Updated: 2026/02/05 01:53:17 by michel           ###   ########.fr       */
+/*   Updated: 2026/02/05 02:02:15 by michel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,42 @@ int main( void )
 {
     int a = 2;
     int b = 3;
-    
-    std::cout << "befor swap" << std::endl;
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    ::swap( a, b );
-    std::cout << "after swap" << std::endl;
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-    
+
+    std::cout << "before swap\n";
+    std::cout << "a = " << a << ", b = " << b << "\n";
+    ::swap(a, b);
+    std::cout << "after swap\n";
+    std::cout << "a = " << a << ", b = " << b << "\n";
+    std::cout << "min(a,b) = " << ::min(a, b) << "\n";
+    std::cout << "max(a,b) = " << ::max(a, b) << "\n";
+
     std::string c = "chaine1";
     std::string d = "chaine2";
-    
-    std::cout << "befor swap" << std::endl;
-    std::cout << "c = " << c << ", d = " << d << std::endl;
+
+    std::cout << "\nbefore swap\n";
+    std::cout << "c = " << c << ", d = " << d << "\n";
     ::swap(c, d);
-    std::cout << "after swap" << std::endl;
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    
+    std::cout << "after swap\n";
+    std::cout << "c = " << c << ", d = " << d << "\n";
+    std::cout << "min(c,d) = " << ::min(c, d) << "\n";
+    std::cout << "max(c,d) = " << ::max(c, d) << "\n";
+
+    // Equality test (rule: if equal, return the second)
+    int x = 42;
+    int y = 42;
+    std::cout << "\nEQUALITY TEST\n";
+    std::cout << "x = " << x << ", y = " << y << "\n";
+    std::cout << "min(x,y) = " << ::min(x, y) << " (must be y)\n";
+    std::cout << "max(x,y) = " << ::max(x, y) << " (must be y)\n";
+    std::cout << "&min(x,y) == &y ? " << (&::min(x, y) == &y) << "\n";
+    std::cout << "&max(x,y) == &y ? " << (&::max(x, y) == &y) << "\n";
+
+    // Const test
+    const int ca = 10;
+    const int cb = 20;
+    std::cout << "\nCONST TEST\n";
+    std::cout << "min(ca,cb) = " << ::min(ca, cb) << "\n";
+    std::cout << "max(ca,cb) = " << ::max(ca, cb) << "\n";
+
     return 0;
 }
